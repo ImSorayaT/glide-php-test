a92b4e1e0633fd66f54c83e3eafe72a9200074b8
imsorayat <sorayataraszka@gmail.com>
2022-07-13 17:14:29 +0100

    turn the string into an array

--- index.php
+++ index.php
@@ -23,26 +23,58 @@ class caloricValue {
             $first_day_month_formatted = date_format($first_day_month, 'Y-m-d\TH:i:s');
             $last_day_month_formatted = date_format($last_day_month, 'Y-m-d\TH:i:s');
 
-            // echo $first_day_month_formatted.'<br>';
-            // echo $last_day_month_formatted.'<br>';
-
             $content .= file_get_contents('http://mip-prd-web.azurewebsites.net/CustomDataDownload?LatestValue=false&Applicable=applicableFor&FromUtcDatetime='.$first_day_month_formatted.'.000Z&ToUtcDateTime='.$last_day_month_formatted.'.000Z&PublicationObjectStagingIds=PUBOBJ1660,PUBOB4507,PUBOB4508,PUBOB4510,PUBOB4509,PUBOB4511,PUBOB4512,PUBOB4513,PUBOB4514,PUBOB4515,PUBOB4516,PUBOB4517,PUBOB4518,PUBOB4519,PUBOB4521,PUBOB4520,PUBOB4522,PUBOBJ1661,PUBOBJ1662');
 
             $i--;
         }
 
-        var_dump($content);
+
+        return $this->stringResultsToArray($content);
 
     }
+
+
+    // turns the string recieved from the site into an array
+    function stringResultsToArray($content){
+
+        $dom = new DOMDocument;
+        @$dom->loadHTML($content);
+
+        $rows = $dom->getElementsByTagName('tr');
+    
+        foreach ($rows as $row){
+            $results[] = $this->tdrows($row->childNodes);
+
+        }
+
+        $header = array_shift($results);
+
+        return $results;
+    }
+
+    // processes each row from the table and turns it into an array
+    function tdrows($elements){
+
+        $keys = array('applicable_at', 'applicable_for', 'data_item', 'value', 'generated_time');
+        
+        foreach ($elements as $element) {
+
+            if($element->tagName == 'td'){
+                $rowArray[] = $element->nodeValue;
+            }
+            $rowArray_with_keys = array_fill_keys($keys, $rowArray);
+
+        }
+        $rowArray_with_keys = array_fill_keys($keys, $rowArray[1]);
+
+        return $rowArray_with_keys;
+    }
+    
+
 
 }
 
 $caloric_value = new caloricValue;
 
-$caloric_value->get_report();
+var_dump($caloric_value->get_report());
 
-// $current_month = MONTH($current_day);
-
-
-
-?>
